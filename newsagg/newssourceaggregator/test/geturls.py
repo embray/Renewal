659d0c100c14f85fc65a7642ac2444dea9229d5c
E. Madison Bray <erik.bray@lri.fr>
2019-10-04 14:53:02 +0200

    Use raw string for regexp to avoid invalid escape sequence warning.

--- newsagg/newssourceaggregator/test/geturls.py
+++ newsagg/newssourceaggregator/test/geturls.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     output = open("test_rss_urls.csv", 'w+')
     handle = open("./urls.csv")
     html = handle.read()
-    urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2})|/[\w])+', html)
+    urls = re.findall(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2})|/[\w])+', html)
     final = list(set(urls))
     for url in final:
         print(url)

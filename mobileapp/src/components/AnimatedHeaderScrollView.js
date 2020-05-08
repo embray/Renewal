574b3089034164a76120f018cb35f1f9fc48a0d6
E. Madison Bray <erik.bray@lri.fr>
2020-05-08 16:04:08 +0200

    [app][refactoring] abstracted out the animated header behavior (header shows/hides on scroll) behavior of the Recommandations screen to a higher-order component
    this makes the implementation of that screen much cleaner, as it's possible
    to define the Header and Content components cleanly without mixing them up
    with the more complicated code that makes the animated header behavior work
    
    now this behavior can more easily be used on other screens too (such as the
    article view)

---
+++ mobileapp/src/components/AnimatedHeaderScrollView.js
@@ -0,0 +1,132 @@
+// This higher order component is a generalization of the Recommendations
+// component which has a Header that hides/shows as the user scrolls through
+// the view.
+//
+// It takes a Header Component and a Content Component (which should be a
+// ScrollView or a Component derived from ScrollView) and wraps the whole
+// thing in a StackNavigator with a header that hides/shows during scroll.
+
+import { variables as ThemeVariables } from 'native-base';
+import React, { Component } from 'react';
+import { Animated, Dimensions, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+export default function AnimatedHeaderScrollView(screenName, Header, Content) {
+  class AnimatedHeaderHeaderWrapper extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+      const { translateYAnim } = this.props;
+      //const headerTop = headerExtraParams.headerTop;
+      const width = Dimensions.get('window').width;
+      //const translateY = (headerTop !== undefined ? headerTop : 0);
+      const translateY = (translateYAnim !== undefined ? translateYAnim : 0);
+      // Modify the flexbox for the main heading to put the title in
+      // the center
+      const style = [ styles.headerContainer, { width, translateY } ];
+
+      return (
+        <Animated.View style={ style }>
+          <Header { ...this.props } />
+        </Animated.View>
+      );
+    }
+  }
+
+  class AnimatedHeaderContentWrapper extends Component {
+    constructor(props) {
+      super(props);
+
+      // This is a fixed constant hard-coded in native-base.
+      // Here we are just using the default theme.
+      this.headerHeight = ThemeVariables.toolbarHeight;
+      this.scrollYAnim = new Animated.Value(0);
+
+      // Using Animated.diffClamp ensures that the raw scroll
+      // animation value (the contentOffset.y of the page) is
+      // simply the difference between scrollYAnim's current and previous values
+      // clamped to the range [0, headerHeight]; the interpolation then
+      // merely inverts the value to set the header translation
+      this.headerTranslateYAnim = Animated.diffClamp(
+        this.scrollYAnim, 0, this.headerHeight).interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -1]
+        });
+    }
+
+    render() {
+      // Note: The paddingTop: ThemeVariables.toolbarHeight is crucial to
+      // ensure that the top of the Content is not overlapped by the animated
+      // header; I cannot figure out exactly why this is where the padding is
+      // needed and why, say, a margin wouldn't work instead, but this seems to
+      // do the trick.
+      //
+      // Note: overScrollMode prevents a 'bounce' on android when reaching the
+      // top of the last that can cause the header to get bumped out of place a
+      // bit; can't figure out how to prevent that otherwise.  the center
+      //
+      // Hack!!  We include the Header here *after* ArticlesList since it's an
+      // absolutely positioned element and can't be interacted with otherwise.
+      // See note the AnimatedHeaderStackNavigator.render() method for more
+      // details.
+      return (
+        <>
+          <Content { ...this.props }
+            onScroll={ Animated.event([{
+              nativeEvent: {contentOffset: {y: this.scrollYAnim}}
+            }], { useNativeDriver: true }) }
+            scrollEventThrottle={ 16 }
+            overScrollMode={ 'never' }
+            contentContainerStyle={{ paddingTop: this.headerHeight }}
+          />
+          <AnimatedHeaderHeaderWrapper
+            route={ this.props.route }
+            navigation={ this.props.navigation }
+            translateYAnim={ this.headerTranslateYAnim }
+          />
+        </>
+      );
+    }
+  }
+
+  // In order to add a header to DrawerNavigator screens it's apparently
+  // necessary to wrap each Screen in its own StackNavigator, which is
+  // a bit of a mess...
+
+  // https://github.com/react-navigation/react-navigation/issues/1632
+  // Not sure why DrawerNavigator screens can't also just have headers...
+  const Stack = createStackNavigator();
+
+  class AnimatedHeaderStackNavigator extends Component {
+    render() {
+      // Hack: We set the navigation header to null here, which just leaves a
+      // blank space in the header content area.  The header content will be
+      // filled by RecommendationsHeader, but because it is absolutely
+      // positioned in order to make the animation look good, it cannot receive
+      // touch events unless it is actually lower in the DOM than other
+      // components.  Basically absolutely positioned elements get implicit
+      // negative zorder than can't be overridden unless they are positioned
+      // later in the DOM--this is true of web development as well.
+      return (
+        <Stack.Navigator screenOptions={{ header: (props) => null }}>
+          <Stack.Screen name={ screenName }>
+            { (props) => <AnimatedHeaderContentWrapper { ...props } /> }
+          </Stack.Screen>
+        </Stack.Navigator>
+      );
+    }
+  }
+
+  return AnimatedHeaderStackNavigator;
+}
+
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'absolute',
+    overflow: 'hidden'
+  }
+});
--- mobileapp/src/containers/Recommendations.js
+++ mobileapp/src/containers/Recommendations.js
@@ -5,22 +5,13 @@ import {
   Icon,
   Left,
   Right,
-  Title,
-  variables as ThemeVariables
+  Title
 } from 'native-base';
 import React, { Component } from 'react';
-import { Animated, Dimensions, StyleSheet } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet } from 'react-native';
 
+import AnimatedHeaderScrollView from '../components/AnimatedHeaderScrollView';
 import ArticlesList from './ArticlesList';
-
-// In order to add a header to DrawerNavigator screens it's apparently
-// necessary to wrap each Screen in its own StackNavigator, which is
-// a bit of a mess...
-
-// https://github.com/react-navigation/react-navigation/issues/1632
-// Not sure why DrawerNavigator screens can't also just have headers...
-const RecommendationsStack = createStackNavigator();
 
 
 // This is the "main" home screen header.
@@ -30,126 +21,42 @@ class RecommendationsHeader extends Component {
   }
 
   render() {
-    const { name } = this.props.route;
-    const { translateYAnim } = this.props;
-    //const headerTop = headerExtraParams.headerTop;
-    const width = Dimensions.get('window').width;
-    //const translateY = (headerTop !== undefined ? headerTop : 0);
-    const translateY = (translateYAnim !== undefined ? translateYAnim : 0);
-    const transform = [{ translateY }];
     // Modify the flexbox for the main heading to put the title in
     // the center
     const flexStyle = {'flex': 1, 'justifyContent': 'center'};
 
     return (
-      <Animated.View style={[ styles.headerContainer, { width, translateY } ]}>
-        <Header style={ styles.header }>
-          <Left style={ flexStyle }>
-            <Button transparent onPress={ this._onMenuButtonPress.bind(this) }>
-              <Icon name='menu' style={{ color: '#fff'}} />
-            </Button>
-          </Left>
-          <Body style={ [styles.headerBody, flexStyle] }>
-            <Title style={ styles.headerTitle }>RENEWAL</Title>
-          </Body>
-          <Right style={ flexStyle }>
-          </Right>
-        </Header>
-      </Animated.View>
-    );
-  }
-}
-
-
-// Contains the StackNavigator for the home screen which is
-// embedded in the DrawerNavigator
-export default class Recommendations extends Component {
-  render() {
-    // Hack: We set the navigation header to null here, which just leaves
-    // a blank space in the header content area.  The header content will
-    // be filled by RecommendationsHeader, but because it is absolutely
-    // positioned in order to make the animation look good, it cannot
-    // receive touch events unless it is actually lower in the DOM than
-    // other components.  Basically absolutely positioned elements get
-    // implicit negative zorder than can't be overridden unless they are
-    // positioned later in the DOM--this is true of web development as well.
-    return (
-      <RecommendationsStack.Navigator
-        screenOptions={{ header: (props) => null }}
-      >
-        <RecommendationsStack.Screen name="recommendations"
-          component={ RecommendationsContent }
-        />
-      </RecommendationsStack.Navigator>
+      <Header style={ styles.header }>
+        <Left style={ flexStyle }>
+          <Button transparent onPress={ this._onMenuButtonPress.bind(this) }>
+            <Icon name='menu' style={{ color: '#fff'}} />
+          </Button>
+        </Left>
+        <Body style={ [styles.headerBody, flexStyle] }>
+          <Title style={ styles.headerTitle }>RENEWAL</Title>
+        </Body>
+        <Right style={ flexStyle }>
+        </Right>
+      </Header>
     );
   }
 }
 
 
 class RecommendationsContent extends Component {
-  constructor(props) {
-    super(props);
-
-    // This is a fixed constant hard-coded in native-base.
-    // Here we are just using the default theme.
-    this.headerHeight = ThemeVariables.toolbarHeight;
-    this.scrollYAnim = new Animated.Value(0);
-
-    // Using Animated.diffClamp ensures that the raw scroll
-    // animation value (the contentOffset.y of the page) is
-    // simply the difference between scrollYAnim's current and previous values
-    // clamped to the range [0, headerHeight]; the interpolation then
-    // merely inverts the value to set the header translation
-    this.headerTranslateYAnim = Animated.diffClamp(
-      this.scrollYAnim, 0, this.headerHeight).interpolate({
-        inputRange: [0, 1],
-        outputRange: [0, -1]
-      });
-  }
-
   render() {
-    // Note: The paddingTop: ThemeVariables.toolbarHeight
-    // is crucial to ensure that the top of the ArticlesList
-    // is not overlapped by the animated header; I cannot
-    // figure out exactly why this is where the padding is needed
-    // and why, say, a margin wouldn't work instead, but this seems
-    // to do the trick.
-    //
-    // Note: overScrollMode prevents a 'bounce' on android when reaching
-    // the top of the last that can cause the header to get bumped out of
-    // place a bit; can't figure out how to prevent that otherwise.
-    // the center
-    //
-    // Hack!!  We include RecommendationsHeader here *after* ArticlesList
-    // since it's an absolutely positioned element and can't be interacted
-    // with otherwise.  See note the Recommendations.render() method for more
-    // details.
     return (
-      <>
-        <ArticlesList { ...this.props }
-          listName={ 'recommendations' }
-          onScroll={ Animated.event([{
-            nativeEvent: {contentOffset: {y: this.scrollYAnim}}
-          }], { useNativeDriver: true }) }
-          scrollEventThrottle={ 16 }
-          overScrollMode={ 'never' }
-          contentContainerStyle={{ paddingTop: this.headerHeight }}
-        />
-        <RecommendationsHeader route={ this.props.route }
-          navigation={ this.props.navigation }
-          translateYAnim={ this.headerTranslateYAnim }
-        />
-      </>
+      <ArticlesList { ...this.props } listName={ 'recommendations' } />
     );
   }
 }
 
 
+export default  AnimatedHeaderScrollView(
+  'recommendations', RecommendationsHeader, RecommendationsContent);
+
+
 const styles = StyleSheet.create({
-  headerContainer: {
-    position: 'absolute',
-    overflow: 'hidden'
-  },
   header: {
     backgroundColor: '#212121',
   },
